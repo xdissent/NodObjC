5de624592a23d4db899fd316d1a2858f1f5318f8
Nathan Rajlich <nathan@tootallnate.net>
2012-05-12 20:07:34 -0700

    test: update core/getClassList test for node-ffi v1.0

--- test/core/getClassList.js
+++ test/core/getClassList.js
@@ -1,4 +1,5 @@
 var b = require('../../lib/core')
+  , ref = require('ref')
   , assert = require('assert')
 
 
@@ -10,18 +11,17 @@ var numClasses = b.objc_getClassList(null, 0)
 assert.ok(numClasses > 0)
 
 if (numClasses > 0) {
-  var sizeofClass = 8 // need a good way to sizeof for real
-    , classes = new b.Pointer(sizeofClass * numClasses)
+  var sizeofClass = ref.sizeof.pointer
+    , classes = new Buffer(sizeofClass * numClasses)
     , cursor = classes
 
   b.objc_getClassList(classes, numClasses)
 
   for (var i=0; i<numClasses; i++) {
-    var c = cursor.getPointer()
-      , name = b.class_getName(c)
+    var c = cursor.readPointer(sizeofClass * i)
+    var name = b.class_getName(c)
     assert.equal(typeof name, 'string')
     assert.ok(name.length > 0)
-    cursor = cursor.seek(sizeofClass)
   }
 
 }
