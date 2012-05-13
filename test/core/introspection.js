9d09f2020150c02df55a163bf1b1fdcd54c12318
Nathan Rajlich <nathan@tootallnate.net>
2012-05-12 20:13:44 -0700

    test: update the core/introspection test to pass

--- test/core/introspection.js
+++ test/core/introspection.js
@@ -1,5 +1,6 @@
 var b = require('../../lib/core')
   , ffi = require('node-ffi')
+  , ref = require('ref')
   , assert = require('assert')
   , className = 'NSMutableArray'
 
@@ -12,19 +13,19 @@ assert.equal(typeof name, 'string')
 assert.equal(name, className)
 //console.error('class_getName: %s', name)
 
-var numMethods = new ffi.Pointer(ffi.Bindings.TYPE_SIZE_MAP.uint32)
+var numMethods = ref.alloc(ref.types.uint32)
   , methods = b.class_copyMethodList(c, numMethods)
   , p = methods
-numMethods = numMethods.getUInt32()
+numMethods = numMethods.deref()
 assert.ok(numMethods > 0)
 
 for (var i=0; i<numMethods; i++) {
-  var cur = p.getPointer()
+  var cur = p.readPointer(i * ref.sizeof.pointer)
     , name = b.sel_getName(b.method_getName(cur))
     , numArgs = b.method_getNumberOfArguments(cur)
     , r = b.method_copyReturnType(cur)
-    , rtn = r.getCString()
-  ffi.free(r)
+    , rtn = r.readCString()
+  b.free(r)
   assert.equal(typeof rtn, 'string')
   assert.ok(numArgs >= 2)
   //console.error('  '+name)
@@ -32,17 +33,14 @@ for (var i=0; i<numMethods; i++) {
   // the first two args are always the id and the SEL
   for (var j=2; j<numArgs; j++) {
     var a = b.method_copyArgumentType(cur, j)
-      , s = a.getCString()
-    ffi.free(a)
+      , s = a.readCString()
+    b.free(a)
     assert.equal(typeof s, 'string')
     assert.ok(s.length > 0)
     //console.error('      Arg %d: %s', j-2, s)
   }
-
-  // advance the cursor
-  p = p.seek(ffi.Bindings.TYPE_SIZE_MAP.pointer)
 }
-ffi.free(methods)
+b.free(methods)
 
 
 // Walk the inheritance chain
