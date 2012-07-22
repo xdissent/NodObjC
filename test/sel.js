f5cdd1f3af308906ea0ae974fa6ce8ee3d982837
Nathan Rajlich <nathan@tootallnate.net>
2012-07-22 11:34:29 -0700

    sel: rewrite SEL to be self-contained and not depend on "core"
    also, complies with the ref "type" interface nicely now

---
+++ test/sel.js
@@ -0,0 +1,18 @@
+
+var assert = require('assert')
+var SEL = require('../lib/sel')
+
+// cache should be empty
+assert.equal(0, Object.keys(SEL.cache).length)
+
+var sel = 'stringWithUTF8String:'
+var swus = SEL.toSEL(sel)
+
+// cache should have 1 entry
+assert.equal(1, Object.keys(SEL.cache).length)
+
+// should return a cached reference
+assert.strictEqual(swus, SEL.toSEL(sel))
+
+// should match the input string when "toString()" is called
+assert.equal(sel, SEL.toString(swus))
